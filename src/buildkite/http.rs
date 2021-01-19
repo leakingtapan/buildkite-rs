bc360154877f0b2d145db71ee28e7c1b0ef37a90
Cheng Pan <pancheng1987@gmail.com>
2021-01-19 14:13:19 -0800

    Refactor the code for each invidual services

---
+++ src/buildkite/http.rs
@@ -0,0 +1,38 @@
+use reqwest::blocking;
+use serde::de::DeserializeOwned;
+
+use crate::buildkite::types::Result;
+
+// HttpClient is for handling http requests to Buildkite API
+pub struct HttpClient {
+
+    // internal http client
+    client: blocking::Client,
+
+    // buildkite API token
+    token: String
+}
+
+impl HttpClient {
+    pub fn new(token: String) -> HttpClient {
+        HttpClient {
+            client: blocking::Client::new(),
+            token: token
+        }
+    }
+
+    /// generic function to fetch the response and deserialize to struct of given type
+    pub fn get_response<T: DeserializeOwned>(&self, url: &str) -> Result<T> {
+        self.client
+            .get(url)
+            .bearer_auth(self.token.as_str())
+            .send()?
+            .json::<T>()
+    }
+}
+
+pub const BUILDKITE_URL_BASE: &str = "https://api.buildkite.com/v2";
+
+pub fn base_url(org: &str) -> String {
+    format!("{}/organizations/{}", BUILDKITE_URL_BASE, org)
+}
